6fbc27122e079afd8169d3bfd659d0a464045c9d
Pritam <180375336+spriyatam28@users.noreply.github.com>
2025-05-20 10:27:29 +0530

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Pritam on 20/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
